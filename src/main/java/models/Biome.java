056cb39a26e486e5b62b3cefefe3d053b2e1d2f1
InesHAMIDI <ines.hamidi@etu.univ-amu.fr>
2023-02-15 11:48:32 +0100

    we prefer it to be a Maven Project

---
+++ src/main/java/models/Biome.java
@@ -0,0 +1,16 @@
+package main.java.models;
+
+public class Biome {
+	
+	private int id;
+
+    private String name;
+    private String category;
+    private float temperature;
+    private String precipitation;
+    private String dimension;
+    private String displayName;
+    private String color;
+    private float rainfall;
+
+}
